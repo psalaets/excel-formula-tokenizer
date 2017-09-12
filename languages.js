99fe1330127335c855f39304109657b7a3700f05
Paul Salaets <psalaets@gmail.com>
2017-09-11 21:43:34 -0400

    try to explain what is what

--- languages.js
+++ languages.js
@@ -1,9 +1,14 @@
 module.exports = {
   'en-US': {
+    // value for true
     true: 'TRUE',
+    // value for false
     false: 'FALSE',
+    // separates function arguments
     argumentSeparator: ',',
+    // decimal point in numbers
     decimalSeparator: '.',
+    // returns number string that can be parsed by Number()
     reformatNumberForJsParsing: function(n) {return n;}
   },
   'de-DE': {
