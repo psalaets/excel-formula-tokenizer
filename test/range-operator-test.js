7cd0856716c0947ff163da0a579a5985bf385e04
Jordan Laramie <jordanlaramie@gmail.com>
2021-12-26 16:55:01 -0500

    Adding additional tests for checking with EU syntax differences (,;\.)

--- test/range-operator-test.js
+++ test/range-operator-test.js
@@ -26,6 +26,78 @@ describe('range operators', function () {
         [' ', 'operator-infix', 'intersect'],
         ['A2:C2', 'operand', 'range']
       ]
+    ],
+    [
+      '={{1.1,1.2};{1.1,1.2}}',
+      [
+        ['ARRAY', 'function', 'start'],
+        // First Column
+        ['ARRAYROW', 'function', 'start'],
+        ['ARRAY', 'function', 'start'],
+        ['ARRAYROW', 'function', 'start'],
+        ['1.1', 'operand', 'number'],
+        [',', 'argument', ''],
+        ['1.2', 'operand', 'number'],
+        ['', 'function', 'stop'],
+        ['', 'function', 'stop'],
+        ['', 'function', 'stop'],
+        // Divider
+        [',', 'argument', ''],
+        // Second Column1
+        ['ARRAYROW', 'function', 'start'],
+        ['ARRAY', 'function', 'start'],
+        ['ARRAYROW', 'function', 'start'],
+        ['1.1', 'operand', 'number'],
+        [',', 'argument', ''],
+        ['1.2', 'operand', 'number'],
+        ['', 'function', 'stop'],
+        ['', 'function', 'stop'],
+        ['', 'function', 'stop'],
+        // End
+        ['', 'function', 'stop']
+      ]
     ]
   ].forEach(itBlock);
+
+  describe('i18n', function () {
+    describe('de-DE', function () {
+      var options = {
+        language: 'de-DE'
+      };
+
+      [
+        [
+          '={{1,1;1,2}\\{1,1;1,2}}',
+          [
+            ['ARRAY', 'function', 'start'],
+            // First Column
+            ['ARRAYROW', 'function', 'start'],
+            ['ARRAY', 'function', 'start'],
+            ['ARRAYROW', 'function', 'start'],
+            ['1.1', 'operand', 'number'],
+            [',', 'argument', ''],
+            ['1.2', 'operand', 'number'],
+            ['', 'function', 'stop'],
+            ['', 'function', 'stop'],
+            ['', 'function', 'stop'],
+            // Divider
+            [',', 'argument', ''],
+            // Second Column1
+            ['ARRAYROW', 'function', 'start'],
+            ['ARRAY', 'function', 'start'],
+            ['ARRAYROW', 'function', 'start'],
+            ['1.1', 'operand', 'number'],
+            [',', 'argument', ''],
+            ['1.2', 'operand', 'number'],
+            ['', 'function', 'stop'],
+            ['', 'function', 'stop'],
+            ['', 'function', 'stop'],
+            // End
+            ['', 'function', 'stop']
+          ],
+          options
+        ]
+      ].forEach(itBlock);
+    });
+  });
 });
