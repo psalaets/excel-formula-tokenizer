38a981f04e17525dab5393f34c69012adc037512
Jordan Laramie <jordanlaramie@gmail.com>
2021-12-26 20:45:49 -0500

    Moving new tests into arrays-test and expanded it to test both combining 2 ranges as rows and as columns

--- test/arrays-test.js
+++ test/arrays-test.js
@@ -3,36 +3,81 @@ const { itBlock } = require('./test-helper');
 describe('arrays', function () {
   [
     [
-      '{1,2,3}',
+      '{1.1,2.1,3.1}',
       [
         ['ARRAY', 'function', 'start'],
         ['ARRAYROW', 'function', 'start'],
-        ['1', 'operand', 'number'],
+        ['1.1', 'operand', 'number'],
         [',', 'argument', ''],
-        ['2', 'operand', 'number'],
+        ['2.1', 'operand', 'number'],
         [',', 'argument', ''],
-        ['3', 'operand', 'number'],
+        ['3.1', 'operand', 'number'],
         ['', 'function', 'stop'],
         ['', 'function', 'stop']
       ]
     ],
     [
-      '{1;2;3}',
+      '{1.1;2.1;3.1}',
       [
         ['ARRAY', 'function', 'start'],
         ['ARRAYROW', 'function', 'start'],
-        ['1', 'operand', 'number'],
+        ['1.1', 'operand', 'number'],
         ['', 'function', 'stop'],
         [',', 'argument', ''],
         ['ARRAYROW', 'function', 'start'],
-        ['2', 'operand', 'number'],
+        ['2.1', 'operand', 'number'],
         ['', 'function', 'stop'],
         [',', 'argument', ''],
         ['ARRAYROW', 'function', 'start'],
-        ['3', 'operand', 'number'],
+        ['3.1', 'operand', 'number'],
         ['', 'function', 'stop'],
         ['', 'function', 'stop']
       ]
     ]
   ].forEach(itBlock);
+
+  describe('i18n', function () {
+    describe('de-DE', function () {
+      var options = {
+        language: 'de-DE'
+      };
+
+      [
+        [
+          '{1,1.2,1.3,1}',
+          [
+            ['ARRAY', 'function', 'start'],
+            ['ARRAYROW', 'function', 'start'],
+            ['1.1', 'operand', 'number'],
+            [',', 'argument', ''],
+            ['2.1', 'operand', 'number'],
+            [',', 'argument', ''],
+            ['3.1', 'operand', 'number'],
+            ['', 'function', 'stop'],
+            ['', 'function', 'stop']
+          ],
+          options
+        ],
+        [
+          '{1,1;2,1;3,1}',
+          [
+            ['ARRAY', 'function', 'start'],
+            ['ARRAYROW', 'function', 'start'],
+            ['1.1', 'operand', 'number'],
+            ['', 'function', 'stop'],
+            [',', 'argument', ''],
+            ['ARRAYROW', 'function', 'start'],
+            ['2.1', 'operand', 'number'],
+            ['', 'function', 'stop'],
+            [',', 'argument', ''],
+            ['ARRAYROW', 'function', 'start'],
+            ['3.1', 'operand', 'number'],
+            ['', 'function', 'stop'],
+            ['', 'function', 'stop']
+          ],
+          options
+        ]
+      ].forEach(itBlock);
+    });
+  });
 });
