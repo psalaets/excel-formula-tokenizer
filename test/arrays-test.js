b49438ef4192f4918183004c92d8056e8d4ede76
Jordan Laramie <jordanlaramie@gmail.com>
2022-01-01 17:47:26 -0500

    Adding tests for advanced array literals found in google sheets

--- test/arrays-test.js
+++ test/arrays-test.js
@@ -33,6 +33,70 @@ describe('arrays', function () {
         ['ARRAYROW', 'function', 'stop'],
         ['ARRAY', 'function', 'stop']
       ]
+    ],
+    // I don't think excel supports this at all but google sheets does
+    [
+      '{{1.1,1.2};{1.3,1.4}}',
+      [
+        ['ARRAY', 'function', 'start'],
+        // First Row
+        ['ARRAYROW', 'function', 'start'],
+        ['ARRAY', 'function', 'start'],
+        ['ARRAYROW', 'function', 'start'],
+        ['1.1', 'operand', 'number'],
+        [',', 'argument', ''],
+        ['1.2', 'operand', 'number'],
+        ['ARRAYROW', 'function', 'stop'],
+        ['ARRAY', 'function', 'stop'],
+        ['ARRAYROW', 'function', 'stop'],
+        // Divider
+        [',', 'argument', ''],
+        // Second Row
+        ['ARRAYROW', 'function', 'start'],
+        ['ARRAY', 'function', 'start'],
+        ['ARRAYROW', 'function', 'start'],
+        ['1.3', 'operand', 'number'],
+        [',', 'argument', ''],
+        ['1.4', 'operand', 'number'],
+        ['ARRAYROW', 'function', 'stop'],
+        ['ARRAY', 'function', 'stop'],
+        ['ARRAYROW', 'function', 'stop'],
+        // End
+        ['ARRAY', 'function', 'stop']
+      ]
+    ],
+    // I don't think excel supports this at all but google sheets does
+    [
+      '{{1.1;1.2},{1.3;1.4}}',
+      [
+        ['ARRAY', 'function', 'start'],
+        ['ARRAYROW', 'function', 'start'],
+        // First Column
+        ['ARRAY', 'function', 'start'],
+        ['ARRAYROW', 'function', 'start'],
+        ['1.1', 'operand', 'number'],
+        ['ARRAYROW', 'function', 'stop'],
+        [',', 'argument', ''],
+        ['ARRAYROW', 'function', 'start'],
+        ['1.2', 'operand', 'number'],
+        ['ARRAYROW', 'function', 'stop'],
+        ['ARRAY', 'function', 'stop'],
+        // Divider
+        [',', 'argument', ''],
+        // Second Column
+        ['ARRAY', 'function', 'start'],
+        ['ARRAYROW', 'function', 'start'],
+        ['1.3', 'operand', 'number'],
+        ['ARRAYROW', 'function', 'stop'],
+        [',', 'argument', ''],
+        ['ARRAYROW', 'function', 'start'],
+        ['1.4', 'operand', 'number'],
+        ['ARRAYROW', 'function', 'stop'],
+        ['ARRAY', 'function', 'stop'],
+        // End
+        ['ARRAYROW', 'function', 'stop'],
+        ['ARRAY', 'function', 'stop']
+      ]
     ]
   ].forEach(itBlock);
 
@@ -72,6 +136,72 @@ describe('arrays', function () {
             [',', 'argument', ''],
             ['ARRAYROW', 'function', 'start'],
             ['3.1', 'operand', 'number'],
+            ['ARRAYROW', 'function', 'stop'],
+            ['ARRAY', 'function', 'stop']
+          ],
+          options
+        ],
+        // I don't think excel supports this at all but google sheets does
+        [
+          '{{1,1.1,2};{1,3.1,4}}',
+          [
+            ['ARRAY', 'function', 'start'],
+            // First Row
+            ['ARRAYROW', 'function', 'start'],
+            ['ARRAY', 'function', 'start'],
+            ['ARRAYROW', 'function', 'start'],
+            ['1.1', 'operand', 'number'],
+            [',', 'argument', ''],
+            ['1.2', 'operand', 'number'],
+            ['ARRAYROW', 'function', 'stop'],
+            ['ARRAY', 'function', 'stop'],
+            ['ARRAYROW', 'function', 'stop'],
+            // Divider
+            [',', 'argument', ''],
+            // Second Row
+            ['ARRAYROW', 'function', 'start'],
+            ['ARRAY', 'function', 'start'],
+            ['ARRAYROW', 'function', 'start'],
+            ['1.3', 'operand', 'number'],
+            [',', 'argument', ''],
+            ['1.4', 'operand', 'number'],
+            ['ARRAYROW', 'function', 'stop'],
+            ['ARRAY', 'function', 'stop'],
+            ['ARRAYROW', 'function', 'stop'],
+            // End
+            ['ARRAY', 'function', 'stop']
+          ],
+          options
+        ],
+        // I don't think excel supports this at all but google sheets does
+        [
+          '{{1,1;1,2}.{1,3;1,4}}',
+          [
+            ['ARRAY', 'function', 'start'],
+            ['ARRAYROW', 'function', 'start'],
+            // First Column
+            ['ARRAY', 'function', 'start'],
+            ['ARRAYROW', 'function', 'start'],
+            ['1.1', 'operand', 'number'],
+            ['ARRAYROW', 'function', 'stop'],
+            [',', 'argument', ''],
+            ['ARRAYROW', 'function', 'start'],
+            ['1.2', 'operand', 'number'],
+            ['ARRAYROW', 'function', 'stop'],
+            ['ARRAY', 'function', 'stop'],
+            // Divider
+            [',', 'argument', ''],
+            // Second Column
+            ['ARRAY', 'function', 'start'],
+            ['ARRAYROW', 'function', 'start'],
+            ['1.3', 'operand', 'number'],
+            ['ARRAYROW', 'function', 'stop'],
+            [',', 'argument', ''],
+            ['ARRAYROW', 'function', 'start'],
+            ['1.4', 'operand', 'number'],
+            ['ARRAYROW', 'function', 'stop'],
+            ['ARRAY', 'function', 'stop'],
+            // End
             ['ARRAYROW', 'function', 'stop'],
             ['ARRAY', 'function', 'stop']
           ],
