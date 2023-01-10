8dd7ced14fa2b460539e2c3e0fd7060cd2c9f54a
Johannes Boyne <johannes@boyne.de>
2023-01-10 10:31:25 +0100

    Add further options to the Options interface

--- index.d.ts
+++ index.d.ts
@@ -6,6 +6,8 @@ export interface Token {
 
 export interface Options {
   language?: 'en-US' | 'de-DE' | 'en-EU';
+  preserveLanguage?: boolean;
+  asClass?: boolean;
 }
 
 declare function tokenize(str: string, options?: Options): Token[];
